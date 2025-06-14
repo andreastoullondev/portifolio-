8d0869cf5b16957ae6d5dc7342250b21ecb1d78b
Andreas Toullon <andreasgitinit@gmail.com>
2025-06-14 17:23:01 -0300

    lampOn

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById ('turnOff');
+const lamp = document.getElementById ('lamp');
+
+//conexão dos elemento HTML ao JAVASCRIPT 
+
+turnOn.addEventListener ('click', lampOn)
