95e3cd4cc8815e689333b975d8b9a37816b47893
Andreas Toullon <andreasgitinit@gmail.com>
2025-06-15 11:52:01 -0300

    lampada completa

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+//conexão dos elemento HTML ao JAVASCRIPT 
 const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById ('turnOff');
 const lamp = document.getElementById ('lamp');
 
-//conexão dos elemento HTML ao JAVASCRIPT 
+//funçoes 
+
+function isLampBroken(){
+    
+    return lamp.src.indexOf ('quebrada') > -1 
+    
+}
 
 function lampOn(){
+    if ( !isLampBroken ()) {
     lamp.src = './img/ligada.jpg'
+     }
 }
 
 function lampOff(){
+      if ( !isLampBroken ()) {
     lamp.src = './img/desligada.jpg'
+      }
 }
 
 function lampBroken(){
     lamp.src = './img/quebrada.jpg'
 }
 
+//elementos 
 turnOn.addEventListener ('click', lampOn)
 turnOff.addEventListener('click', lampOff)
 lamp.addEventListener('mouseover', lampOn);
