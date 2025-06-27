3228b471ffe6558f76ce6e1cf10f925b18898fa7
Andreas Toullon <andreasgitinit@gmail.com>
2025-06-26 23:59:43 -0300

    loop

--- javascript/udemy.js
+++ javascript/udemy.js
@@ -1,7 +1,11 @@
 function pessoas(humanos){
-    console.log('os guri')
+    console.log('os guri',)
 }
-
 for(let dinos = 0; dinos<= 12; dinos++){
     pessoas(dinos);
+}
+let pessoa = 1 
+while(pessoa < 10 ){
+    console.log('nome aleatorio', pessoa, 'Sobrenome aleatorio')
+    pessoa++
 }
