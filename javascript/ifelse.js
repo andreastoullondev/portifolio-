0811dc4abe48ca1d511bc33846fb28b30aa87de9
Andreas Toullon <andreasgitinit@gmail.com>
2025-07-06 23:55:14 -0300

    if elesse

---
+++ javascript/ifelse.js
@@ -0,0 +1,14 @@
+const temperatura = -10
+
+if( temperatura >= 30){
+    console.log("Esta calor");
+}else if(temperatura >= 20){
+    console.log("Temperatura, Perfeita");
+}else{
+    console.log("Congelante");
+}
+
+
+
+
+
