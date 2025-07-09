22f31e75e39f9b37fa66414efa4c1f50caf312f1
Andreas Toullon <andreasgitinit@gmail.com>
2025-07-08 21:20:52 -0300

    tamo indo

--- javascript/ifelse.js
+++ javascript/ifelse.js
@@ -1,11 +1,15 @@
 const pais = "Brasil"
 
-let temperatura = pais === "Brasil" ? 25 : 10
+var mundo = 10 
+
+let temperatura = pais === "Brasil" ? 45 : 10
 
 
 
 if( temperatura >= 30){
-    console.log("Esta calor");
+    for(let contador = 0;contador <=  mundo; contador++ ){
+        console.log(mundo)
+    }
 }else if(temperatura >= 20){
     console.log("Temperatura, Perfeita");
 }else{
