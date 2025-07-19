9472e87b0727a05988604142628a5c676c11133d
Andreas Toullon <andreasgitinit@gmail.com>
2025-07-19 01:12:15 -0300

    pedra papel ou tesoura

--- script.js
+++ script.js
@@ -75,3 +75,25 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 // <----------------------------CONTADOR--------------------------------------->
+
+
+
+
+var elementos = document.querySelectorAll('.player-options div > img');
+var playerOpt = "";
+function resetOpacityPlayer(){
+   for(var i = 0;i < elementos.length; i++){
+        elementos[i].style.opacity = 0;
+   }
+}
+
+for(var i = 0;i < elementos.length; i++){
+    elementos[i].addEventListener('click',function(t){
+        resetOpacityPlayer();
+        t.target.style.opacity = 1;
+       playerOpt = t.target.getAttribute('opt');
+
+       alert(playerOpt)
+    });
+    
+}
