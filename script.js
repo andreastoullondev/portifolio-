1058462283f22ca6433ddbf84349c318ad0d8fc5
Andreas Toullon <andreasgitinit@gmail.com>
2025-07-23 21:40:22 -0300

    tirei todos os var, os var estavam nos loops tirei todos

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ function validarVitoria(){
 
 function resetInimigo(){
     const enemyOptions = document.querySelectorAll('.enemy-options div');
-    for(var i = 0;i < enemyOptions.length; i++){
+    for(let i = 0;i < enemyOptions.length; i++){
        enemyOptions[i].childNodes[0].style.opacity = 0;
     }
 }
@@ -137,7 +137,7 @@ function inimigoJogar(){
     const enemyOptions = document.querySelectorAll('.enemy-options div');
 
     resetInimigo();
-    for(var i = 0;i < enemyOptions.length; i++){
+    for(let i = 0;i < enemyOptions.length; i++){
         
         if(i == rand){
             enemyOptions[i].childNodes[0].style.opacity = 1;
@@ -151,13 +151,13 @@ function inimigoJogar(){
 
 //apos selecionar opção some as outras 
 function resetOpacityPlayer(){
-   for(var i = 0;i < elementos.length; i++){
+   for(let i = 0;i < elementos.length; i++){
         elementos[i].style.opacity = 0;
    }
 }
 
-//Call back 
-for(var i = 0;i < elementos.length; i++){
+//Call back inicio do game 
+for(let i = 0;i < elementos.length; i++){
     elementos[i].addEventListener('click',function(t){
         resetOpacityPlayer();
         t.target.style.opacity = 1;
