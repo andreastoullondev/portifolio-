f7d6760cf1d58d4b9a56cbee95a8190636dba4ef
Andreas Toullon <andreasgitinit@gmail.com>
2025-07-24 23:21:42 -0300

    funçoes

---
+++ javascript/funcoes.js
@@ -0,0 +1,13 @@
+function criaMensagem(){
+    return("Boa noite")
+}
+
+function exibirMensagem(mensagem){
+    console.log(mensagem)
+}
+
+const mensagem = criaMensagem()
+
+exibirMensagem(mensagem);
+
+// FUNÇOES SEM RETORNO 
